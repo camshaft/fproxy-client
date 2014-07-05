565845d2f544da171997f8eed8273f6116346193
camshaft <bytheway.cameron@gmail.com>
2014-07-05 03:05:34 -0600

    fix proxy auth

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ module.exports = function(host) {
 
   if (!host) throw new Error('Missing proxy host');
 
+  var user = process.env.PROXY_USERNAME;
+  var pass = process.env.PROXY_PASSWORD;
+  var auth = user && pass ?
+    'Basic ' + new Buffer(user + ':' + pass).toString('base64') : undefined;
+
   var app = express();
 
   var proxied = proxy(host, {timeout: false});
@@ -13,7 +18,9 @@ module.exports = function(host) {
   app.use(function(req, res, next) {
     req.url = req._parsedUrl.path;
     req.headers['x-fproxy-host'] = req.headers.host;
+    req.headers['x-fproxy-authorization'] = req.headers['proxy-authorization'] || auth;
     req.headers.host = '';
+    delete req.headers['proxy-authorization'];
     proxied(req, res, next);
   });
 
